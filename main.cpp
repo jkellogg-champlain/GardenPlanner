4fc614b454ec2e3ed40e53ce77396a38a566210a
jkellogg-champlain <jeremiah.kellogg@mymail.champlain.edu>
2019-10-02 19:29:25 -0700

    Comment out initial code to allow use of new classes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
-#include <SFML/Graphics.hpp>
+//#include <iostream>
+//#include <SFML/Graphics.hpp>
+#include "Application.h"
 
 int main()
 {
-	sf::RenderWindow mainWindow;
+	Application application;
+	while(!application.GetDisplay()->IsDone())
+	{
+		application.HandleInput();
+		application.Update();
+		application.Render();
+	}
+	/*sf::RenderWindow mainWindow;
 	sf::VideoMode currentMode = sf::VideoMode::getDesktopMode();
 	mainWindow.setFramerateLimit(60);
 	mainWindow.create(sf::VideoMode(currentMode), "Garden Space Planner");
@@ -20,14 +28,14 @@ int main()
 	leftColumn.setPosition(sf::Vector2f((float)((mainWindow.getSize().x * .02)/2), (float)(mainWindow.getSize().y * .12)));
 	leftColumn.setFillColor(sf::Color(86, 225, 58, 255));
 	leftColumn.setOutlineColor(sf::Color(42, 85, 34, 255));
-	leftColumn.setOutlineThickness(2.0f);
+	leftColumn.setOutlineThickness(2.0f);*/
 
 	/*sf::RenderWindow navWindow;
 	navWindow.create(sf::VideoMode(mainWindow.getSize().x * .98, mainWindow.getSize().y * .08), "Nav Window",
 		sf::Style::None);
 	navWindow.setPosition(sf::Vector2i(mainWindow.getPosition().x +(mainWindow.getPosition().x * .08), mainWindow.getPosition().y * .99));*/
 
-	sf::Event event;
+	/*sf::Event event;
 
 	while (mainWindow.isOpen())
 	{
@@ -40,10 +48,10 @@ int main()
 		mainWindow.clear(sf::Color(228, 243, 127, 255));
 		mainWindow.draw(topNavBar);
 		mainWindow.draw(leftColumn);
-		mainWindow.display();
+		mainWindow.display();*/
 		//navWindow.clear(sf::Color(86, 225, 58, 255));
 		//navWindow.display();
 		//std::cout << mainWindow.getSize().x << std::endl;
 		//std::cout << mainWindow.getSize().y << std::endl;
-	}
+	//}
 }
