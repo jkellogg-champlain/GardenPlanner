6456218ff9620a7aa94fe849eccb3566fc25611e
jkellogg-champlain <jeremiah.kellogg@mymail.champlain.edu>
2019-10-12 11:08:11 -0700

    Rework main program to incorporate new ContentContainer Class

--- main.cpp
+++ main.cpp
@@ -1,41 +1,22 @@
 //#include <iostream>
-//#include <SFML/Graphics.hpp>
-#include "Application.h"
+#include <SFML/Graphics.hpp>
+#include "ContentContainer.h"
 
 int main()
 {
-	Application application;
-	while(!application.GetDisplay()->IsDone())
-	{
-		application.HandleInput();
-		application.Update();
-		application.Render();
-	}
-	/*sf::RenderWindow mainWindow;
+	sf::RenderWindow mainWindow;
 	sf::VideoMode currentMode = sf::VideoMode::getDesktopMode();
 	mainWindow.setFramerateLimit(60);
 	mainWindow.create(sf::VideoMode(currentMode), "Garden Space Planner");
 
-	sf::RectangleShape topNavBar;
-	topNavBar.setSize(sf::Vector2f((float)(mainWindow.getSize().x * .98), (float)(mainWindow.getSize().y * .08)));
-	topNavBar.setPosition(sf::Vector2f((float)((mainWindow.getSize().x * .02)/2), (float)(mainWindow.getSize().y * .02)));
-	topNavBar.setFillColor(sf::Color(86, 225, 58, 255));
-	topNavBar.setOutlineColor(sf::Color(42, 85, 34, 255));
-	topNavBar.setOutlineThickness(2.0f);
+	sf::Color background = sf::Color(228, 243, 127, 255);
+	sf::Color foreground = sf::Color(86, 225, 58, 255);
 
-	sf::RectangleShape leftColumn;
-	leftColumn.setSize(sf::Vector2f((float)(mainWindow.getSize().x * .25), (float)(mainWindow.getSize().y * .86)));
-	leftColumn.setPosition(sf::Vector2f((float)((mainWindow.getSize().x * .02)/2), (float)(mainWindow.getSize().y * .12)));
-	leftColumn.setFillColor(sf::Color(86, 225, 58, 255));
-	leftColumn.setOutlineColor(sf::Color(42, 85, 34, 255));
-	leftColumn.setOutlineThickness(2.0f);*/
+	ContentContainer topNavBar(mainWindow, .98f, .08f, .02f, .02f, foreground);
+	ContentContainer leftColumn(mainWindow, .25f, .86f, .02f, .12f, foreground);
+	ContentContainer mainContent(mainWindow, .72f, .86f, .54f, .12f, background);
 
-	/*sf::RenderWindow navWindow;
-	navWindow.create(sf::VideoMode(mainWindow.getSize().x * .98, mainWindow.getSize().y * .08), "Nav Window",
-		sf::Style::None);
-	navWindow.setPosition(sf::Vector2i(mainWindow.getPosition().x +(mainWindow.getPosition().x * .08), mainWindow.getPosition().y * .99));*/
-
-	/*sf::Event event;
+	sf::Event event;
 
 	while (mainWindow.isOpen())
 	{
@@ -45,13 +26,10 @@ int main()
 			mainWindow.close();
 		}
 
-		mainWindow.clear(sf::Color(228, 243, 127, 255));
-		mainWindow.draw(topNavBar);
-		mainWindow.draw(leftColumn);
-		mainWindow.display();*/
-		//navWindow.clear(sf::Color(86, 225, 58, 255));
-		//navWindow.display();
-		//std::cout << mainWindow.getSize().x << std::endl;
-		//std::cout << mainWindow.getSize().y << std::endl;
-	//}
+		mainWindow.clear(background);
+		topNavBar.Draw(mainWindow);
+		leftColumn.Draw(mainWindow);
+		mainContent.Draw(mainWindow);
+		mainWindow.display();
+}
 }
