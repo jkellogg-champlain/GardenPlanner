cd27dcd476cc075319d347ec2a14cee19b20cee5
jkellogg-champlain <jeremiah.kellogg@mymail.champlain.edu>
2019-10-13 12:59:53 -0700

    Edit main program to use new Button Class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-//#include <iostream>
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include "ContentContainer.h"
+#include "Button.h"
 
 int main()
 {
@@ -11,10 +12,28 @@ int main()
 
 	sf::Color background = sf::Color(228, 243, 127, 255);
 	sf::Color foreground = sf::Color(86, 225, 58, 255);
+	sf::Color onHover = sf::Color(238, 244, 177, 255);
 
 	ContentContainer topNavBar(mainWindow, .98f, .08f, .02f, .02f, foreground);
 	ContentContainer leftColumn(mainWindow, .25f, .86f, .02f, .12f, foreground);
 	ContentContainer mainContent(mainWindow, .72f, .86f, .54f, .12f, background);
+
+	sf::Font ubuntu;
+	ubuntu.loadFromFile("Ubuntu-M.ttf");
+
+	Button selectMapBtn("Select Map", {200, 50}, background, sf::Color::Black);
+	selectMapBtn.SetPosition({100, 20});
+	selectMapBtn.SetFont(ubuntu);
+	Button createMapBtn("Create Map", {200, 50}, background, sf::Color::Black);
+	createMapBtn.SetPosition({400, 20});
+	createMapBtn.SetFont(ubuntu);
+	Button addPlantBtn("Add Plant", {200, 50}, background, sf::Color::Black);
+	addPlantBtn.SetPosition({700, 20});
+	addPlantBtn.SetFont(ubuntu);
+	Button editPlantBtn("Edit Plant", {200, 50}, background, sf::Color::Black);
+	editPlantBtn.SetPosition({1000, 20});
+	editPlantBtn.SetFont(ubuntu);
+	Button navButtons [4] = {selectMapBtn, createMapBtn, addPlantBtn, editPlantBtn};
 
 	sf::Event event;
 
@@ -22,12 +41,27 @@ int main()
 	{
 		while (mainWindow.pollEvent (event))
 		{
-			if (event.type == sf::Event::Closed)
-			mainWindow.close();
+			switch(event.type) {
+				case sf::Event::Closed:
+					mainWindow.close();
+					break;
+			}
 		}
 
 		mainWindow.clear(background);
 		topNavBar.Draw(mainWindow);
+		for (Button btn : navButtons)
+		{
+			if(btn.mouseOver(mainWindow))
+			{
+				btn.SetColor(onHover);
+			}
+			else
+			{
+				btn.SetColor(background);
+			}
+			btn.Draw(mainWindow);
+		}
 		leftColumn.Draw(mainWindow);
 		mainContent.Draw(mainWindow);
 		mainWindow.display();
