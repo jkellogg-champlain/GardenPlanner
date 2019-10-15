807d142aaf4b8b4e32068bfdf4b259b92c06747d
EOULib <jkellogg@eou.edu>
2019-10-14 17:39:44 -0700

    Make Button Class more dynamic to display the same on various screen resolutions

--- Button.cpp
+++ Button.cpp
@@ -1,9 +1,9 @@
 #include "Button.h"
 
-Button::Button(std::string name, sf::Vector2f size, sf::Color color, sf::Color textColor)
+Button::Button(std::string name, sf::RenderWindow& window, sf::Color color, sf::Color textColor)
 {
   m_text.setString(name);
-  m_button.setSize(size);
+  m_button.setSize(sf::Vector2f((float)(window.getSize().x * .16f), (float)(window.getSize().y * .06f)));
   m_button.setFillColor(color);
   m_text.setFillColor(textColor);
   m_button.setOutlineColor(sf::Color(42, 85, 34, 255));
@@ -36,8 +36,8 @@ bool Button::mouseOver(sf::RenderWindow &window)
   float btnPosX = m_button.getPosition().x;
   float btnPosY = m_button.getPosition().y;
 
-  float btnXPosWidth = btnPosX + m_button.getLocalBounds().width;
-  float btnYPosHeight = btnPosY + m_button.getLocalBounds().height;
+  float btnXPosWidth = btnPosX + m_button.getGlobalBounds().width;
+  float btnYPosHeight = btnPosY + m_button.getGlobalBounds().height;
 
   if(mouseX < btnXPosWidth && mouseX > btnPosX && mouseY < btnYPosHeight && mouseY > btnPosY)
   {
@@ -46,11 +46,12 @@ bool Button::mouseOver(sf::RenderWindow &window)
   return false;
 }
 
-void Button::SetPosition(sf::Vector2f position)
+void Button::SetPosition(sf::Vector2f area, float ratioX, float ratioY)
 {
+  sf::Vector2f position = {area.x * ratioX, (area.y * ratioY) / 2.5f};
   m_button.setPosition(position);
 
-  float xPos = position.x + ((m_button.getLocalBounds().width - m_text.getLocalBounds().width) / 2) * .25;
-  float yPos = position.y + ((m_button.getLocalBounds().height - m_text.getLocalBounds().height) / 2) * .25;
+  float xPos = position.x + ((m_button.getGlobalBounds().width - m_text.getGlobalBounds().width) / 2) * .25;
+  float yPos = position.y + ((m_button.getGlobalBounds().height - m_text.getGlobalBounds().height) / 2) * .25;
   m_text.setPosition(xPos, yPos);
 }
--- Button.h
+++ Button.h
@@ -1,15 +1,16 @@
 //#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <string>
+//#include "ContentContainer.h"
 
 class Button
 {
 public:
-  Button(std::string name, sf::Vector2f size, sf::Color color, sf::Color textColor);
+  Button(std::string name, sf::RenderWindow& window, sf::Color color, sf::Color textColor);
   ~Button();
 
   void Draw(sf::RenderWindow& window);
-  void SetPosition(sf::Vector2f position);
+  void SetPosition(sf::Vector2f area, float ratioX, float ratioY);
   void SetColor(sf::Color color);
   bool mouseOver(sf::RenderWindow &window);
   void SetFont(sf::Font &font);
