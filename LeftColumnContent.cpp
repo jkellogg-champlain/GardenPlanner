3a825d3d7283f8bb7b8a65e93ea1bd003dcec220
jkellogg-champlain <jeremiah.kellogg@mymail.champlain.edu>
2019-11-10 16:59:59 -0800

    Add functionality to list plants from database in the left column

--- LeftColumnContent.cpp
+++ LeftColumnContent.cpp
@@ -10,6 +10,7 @@ LeftColumnContent::~LeftColumnContent() { }
 
 void LeftColumnContent::AddScrollBar()
 {
+
   m_scrollContainer.setSize({m_leftColumnView.getSize().x * .07f, m_displayArea.getSize().y});
   m_scrollContainer.setPosition({m_displayArea.getPosition().x + (m_displayArea.getSize().x - m_scrollContainer.getSize().x), m_displayArea.getPosition().y});
   m_scrollContainer.setFillColor(sf::Color(220, 220, 220, 255));
@@ -17,15 +18,30 @@ void LeftColumnContent::AddScrollBar()
   m_screenToViewRatio = m_displayArea.getSize().y / m_leftColumnView.getSize().y;
 
   m_scrollElement.setSize({m_scrollContainer.getSize().x * .75f, m_leftColumnView.getSize().y / m_screenToViewRatio});
-  m_scrollMinimum.x = m_scrollContainer.getPosition().x * 1.01f;
+  m_scrollMinimum.x = m_scrollContainer.getPosition().x * 1.015f;
   m_scrollMinimum.y =  m_scrollContainer.getPosition().y;
   m_scrollMaximum.x = m_scrollContainer.getPosition().x * 1.01f;
   m_scrollMaximum.y = m_scrollContainer.getGlobalBounds().height;
   m_scrollElement.setPosition(m_scrollMinimum);
   m_scrollElement.setFillColor(sf::Color(175, 175, 175, 255));
   //std::cout << "Element size is: " << m_scrollElement.getSize().y << std::endl;
-
   m_centerScreen.setPosition(m_leftColumnView.getSize().x / 2.f, (m_leftColumnView.getSize().y / 2.f));
+
+  if(!m_ubuntu.loadFromFile("Ubuntu-M.ttf"))
+  {
+    std::cout << "Unable to load Ubuntu-M.ttf font file" << std::endl;
+  };
+  m_plantTxtName.setFont(m_ubuntu);
+  m_plantTxtName.setFillColor(sf::Color::Black);
+  m_plantTxtVariety.setFont(m_ubuntu);
+  m_plantTxtVariety.setFillColor(sf::Color::Black);
+  m_plantTxtVariety.setCharacterSize(23);
+  m_plantTxtSpacing.setFont(m_ubuntu);
+  m_plantTxtSpacing.setFillColor(sf::Color::Black);
+  m_plantTxtSpacing.setCharacterSize(15);
+  m_plantTxtRow.setFont(m_ubuntu);
+  m_plantTxtRow.setFillColor(sf::Color::Black);
+  m_plantTxtRow.setCharacterSize(15);
 }
 
 void LeftColumnContent::SetFirstClick(bool click)
@@ -56,12 +72,9 @@ void LeftColumnContent::Scroll(sf::RenderWindow &window, sf::RectangleShape &vie
 
   if(m_firstClick)
   {
-    mouseYOld = mouseYNew;
     m_offset = mouseYNew - m_scrollElement.getPosition().y;
   }
 
-  mouseDifference = mouseYNew - mouseYOld;
-  mouseYOld = mouseYNew;
   float viewScrollSpeed = (m_scrollContainer.getSize().y - m_leftColumnView.getSize().y) / (m_scrollContainer.getSize().y - m_scrollElement.getSize().y);
 
   if(mouseYNew - m_offset > m_scrollContainer.getPosition().y && mouseYNew - m_offset < m_scrollContainer.getSize().y - m_scrollElement.getSize().y)
@@ -105,13 +118,12 @@ void LeftColumnContent::ChangeColor(sf::Color color)
 bool LeftColumnContent::MouseOverScroll(sf::RenderWindow &window, sf::RectangleShape &border)
 {
   sf::Vector2i mouseWindowPostion = sf::Mouse::getPosition(window);
-
   sf::Vector2f mouseViewPosition = window.mapPixelToCoords(mouseWindowPostion);
 
   float scrollPosX = m_scrollElement.getPosition().x;
   float scrollXPosWidth = scrollPosX + m_scrollElement.getGlobalBounds().width;
 
-  if(mouseViewPosition.x < scrollXPosWidth && mouseViewPosition.x > scrollPosX && mouseViewPosition.y < m_scrollElement.getPosition().y + m_scrollElement.getSize().y && mouseViewPosition.y > newPosY)
+  if(mouseViewPosition.x < scrollXPosWidth && mouseViewPosition.x > scrollPosX && mouseViewPosition.y < m_scrollElement.getPosition().y + m_scrollElement.getSize().y && mouseViewPosition.y > m_scrollContainer.getPosition().y)
   {
     return true;
   }
@@ -120,11 +132,77 @@ bool LeftColumnContent::MouseOverScroll(sf::RenderWindow &window, sf::RectangleS
 
 void LeftColumnContent::AddDisplayArea()
 {
+  m_plantContainer.setSize({m_leftColumnView.getSize().x - (m_leftColumnView.getSize().x * .07f), m_leftColumnView.getSize().y / 4});
+  m_plantContainer.setFillColor(sf::Color(228, 243, 127, 255));
+  m_plantContainer.setOutlineColor(sf::Color(42, 85, 34, 255));
+  m_plantContainer.setOutlineThickness(1.f);
+
   m_displayArea.setFillColor(sf::Color(228, 243, 127, 255));
-  m_displayArea.setSize({m_leftColumnView.getSize().x * .995f, m_leftColumnView.getSize().y * 3.995f});
+  m_displayArea.setSize({m_leftColumnView.getSize().x * .995f, m_plantContainer.getSize().y * m_plants.size()/*m_leftColumnView.getSize().y * 2.995f*/});
   m_displayArea.setPosition({1, 3});
-  m_displayArea.setOutlineColor(sf::Color::Red);
+  //m_displayArea.setOutlineColor(sf::Color::Red);
   m_displayArea.setOutlineThickness(1.f);
+}
+
+void LeftColumnContent::SetPlantVector()
+{
+  driver = get_driver_instance();
+  con = driver->connect("tcp://127.0.0.1:3306", "garden_planner_user", "spaceplanner");
+  con->setSchema("garden_space_planner");
+  stmt = con->createStatement();
+  res = stmt->executeQuery("SELECT * FROM plants");
+
+  while (res->next()) {
+    /* Access column data by alias or column name */
+    /*std::cout << res->getInt("plant_id") << "   ";
+    std::cout << res->getString("plant_name") << "   ";
+    std::cout << res->getString("plant_variety") << "   ";
+    std::cout << res->getInt("plant_spacing_width") << "   ";
+    std::cout << res->getInt("plant_spacing_length") << std::endl;*/
+
+    /*int id =  res->getInt("plant_id");
+    std::string name = res->getString("plant_name");
+    std::string variety = res->getString("plant_variety");
+    int spacing = res->getInt("plant_spacing_width");
+    int rowspace = res->getInt("plant_spacing_length");*/
+
+    //cout << "\t... MySQL says it again: ";
+    /* Access column data by numeric offset, 1 is the first column */
+    //cout << res->getString(1) << endl;
+    m_plant.SetID(res->getInt("plant_id"));
+    m_plant.SetName(res->getString("plant_name"));
+    m_plant.SetVariety(res->getString("plant_variety"));
+    m_plant.SetSpacing(res->getInt("plant_spacing_width"));
+    m_plant.SetRowSpacing(res->getInt("plant_spacing_length"));
+
+    m_plants.push_back(m_plant);
+  }
+
+  delete res;
+  delete stmt;
+  delete con;
+}
+
+void LeftColumnContent::GetPlants()
+{
+
+}
+
+void LeftColumnContent::DisplayPlants()
+{
+  m_plantDisplayPos = (m_displayArea.getPosition());
+  //float plantBoxPosX = m_displayArea.getPosition().x;
+  //float plantBoxPosY = m_displayArea.getPosition().y;
+  for(int i = 0; i < m_plants.size(); i++)
+  {
+    m_plantDisplayList.push_back(m_plantDisplayPos);
+    /*std::cout << m_plants[i].GetID() << "   ";
+    std::cout << m_plants[i].GetName() << "   ";
+    std::cout << m_plants[i].GetVariety() << "   ";
+    std::cout << m_plants[i].GetSpacing() << "   ";
+    std::cout << m_plants[i].GetRowSpacing() << std::endl;*/
+    m_plantDisplayPos.y += m_plantContainer.getSize().y;
+  }
 }
 
 void LeftColumnContent::Draw(sf::RenderWindow &window)
@@ -132,4 +210,25 @@ void LeftColumnContent::Draw(sf::RenderWindow &window)
   window.draw(m_displayArea);
   window.draw(m_scrollContainer);
   window.draw(m_scrollElement);
+  for(int i = 0; i < m_plantDisplayList.size(); i++)
+  {
+    m_plantContainer.setPosition(m_plantDisplayList[i]);
+
+    m_plantTxtName.setString(m_plants[i].GetName());
+    m_plantTxtName.setPosition((m_plantContainer.getSize().x / 2) - (m_plantTxtName.getGlobalBounds().width / 2), (m_plantDisplayList[i].y + ( m_plantContainer.getSize().y / 4)) - (m_plantTxtName.getGlobalBounds().height));
+
+    m_plantTxtVariety.setString("(" + m_plants[i].GetVariety() + ")");
+    m_plantTxtVariety.setPosition((m_plantContainer.getSize().x / 2) - (m_plantTxtVariety.getGlobalBounds().width / 2), (m_plantDisplayList[i].y + ( m_plantContainer.getSize().y / 3))/* + (m_plantTxtVariety.getGlobalBounds().height)*/);
+
+    std::string numberOfPlants = std::to_string((m_plants[i].GetRowSpacing() * 12) / m_plants[i].GetSpacing());
+    std::string blockSize = std::to_string(m_plants[i].GetRowSpacing());
+    m_plantTxtSpacing.setString(numberOfPlants + " plants per " + blockSize + "x" + blockSize + " foot space");
+    m_plantTxtSpacing.setPosition((m_plantContainer.getSize().x / 2) - (m_plantTxtSpacing.getGlobalBounds().width / 2), (m_plantDisplayList[i].y + ( m_plantContainer.getSize().y / 2)) + (m_plantTxtSpacing.getGlobalBounds().height));
+
+
+    window.draw(m_plantContainer);
+    window.draw(m_plantTxtName);
+    window.draw(m_plantTxtVariety);
+    window.draw(m_plantTxtSpacing);
+  }
 }
--- LeftColumnContent.h
+++ LeftColumnContent.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include "ContentContainer.h"
+#include "Plant.h"
 #include <iostream>
+
+#include "mysql_connection.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/prepared_statement.h>
 
 //#include <SFML/Sprite.hpp>
 
@@ -21,6 +28,9 @@ public:
   bool GetScrolling();
   void ChangeColor(sf::Color color);
   void AddDisplayArea();
+  void SetPlantVector();
+  void GetPlants();
+  void DisplayPlants();
   void Draw(sf::RenderWindow &window);
 
 
@@ -29,20 +39,34 @@ private:
   sf::RectangleShape m_scrollElement;
   sf::Vector2f m_scrollMinimum;
   sf::Vector2f m_scrollMaximum;
-  float mouseY;
-  float mouseX;
+  //float mouseY;
+  //float mouseX;
   float mouseYNew;
-  float mouseYOld;
-  float mouseDifference;
-  float newPosY;
-  float oldPosY;
+  //float mouseYOld;
+  //float mouseDifference;
+  //float newPosY;
+  //float oldPosY;
   float m_offset;
-  float m_startPostion;
+  //float m_startPostion;
   sf::RectangleShape m_centerScreen;
   sf::RectangleShape m_scrollContainer;
   sf::RectangleShape m_displayArea;
+  sf::RectangleShape m_plantContainer;
   bool m_isScrolling;
   bool m_firstClick;
   sf::View m_leftColumnView;
   float m_screenToViewRatio;
+  Plant m_plant;
+  std::vector<Plant> m_plants;
+  std::vector<sf::Vector2f> m_plantDisplayList;
+  sf::Vector2f m_plantDisplayPos;
+  sf::Text m_plantTxtName;
+  sf::Text m_plantTxtVariety;
+  sf::Text m_plantTxtSpacing;
+  sf::Text m_plantTxtRow;
+  sf::Font m_ubuntu;
+  sql::Driver *driver;
+  sql::Connection *con;
+  sql::Statement *stmt;
+  sql::ResultSet *res;
 };
