3a9bd1721071a3073419fd5aa6cf8047d7661a38
EOULib <jkellogg@eou.edu>
2019-11-26 13:01:58 -0800

    Comment the code

--- ContentContainer.cpp
+++ ContentContainer.cpp
@@ -1,5 +1,6 @@
 #include "ContentContainer.h"
 
+//Constructor
 ContentContainer::ContentContainer(sf::RenderWindow& mainWindow, float sizeXRatio, float sizeYRatio, float posXRatio, float posYRatio, sf::Color color)
 {
   m_container.setSize(sf::Vector2f((float)(mainWindow.getSize().x * sizeXRatio), (float)(mainWindow.getSize().y * sizeYRatio)));
@@ -9,18 +10,22 @@ ContentContainer::ContentContainer(sf::RenderWindow& mainWindow, float sizeXRati
 	m_container.setOutlineThickness(2.0f);
 }
 
+//Default Destructor
 ContentContainer::~ContentContainer() { }
 
+//Draws the ContentContainer object to the window.
 void ContentContainer::Draw(sf::RenderWindow& window)
 {
   window.draw(m_container);
 }
 
+//Returns the sf::Vector2f object with the x and y sizes of the container.
 sf::Vector2f ContentContainer::GetSize()
 {
   return m_container.getSize();
 }
 
+//Returns the sf::Vector2f object with the x and y position of the container.
 sf::Vector2f ContentContainer::GetPosition()
 {
   return m_container.getPosition();
--- ContentContainer.h
+++ ContentContainer.h
@@ -5,12 +5,38 @@
 class ContentContainer
 {
 public:
+
+  /**
+    Constructor
+
+  	@param &mainWindow References an sf::RenderWindow object that is used to size the container.
+    @param sizeXRatio The x axis size of the container in relation to the &mainWindow.
+    @param sizeYRatio The y axis size of the container in relation to the &mainWindow.
+    @param posXRatio The x axis position of the container in relation to the &mainWindow.
+    @param posYRatio The y axis position of the container in relation to the &mainWindow.
+  */
   ContentContainer(sf::RenderWindow& mainWindow, float sizeXRatio, float sizeYRatio, float posXRatio, float posYRatio, sf::Color color);
+
+  //Default Destructor
   ~ContentContainer();
 
+  /**
+    Draws the ContentContainer object to the window.
+
+  	@param &window The sf::RenderWindow object the ContentContainer object is to be drawn to.
+  */
   void Draw(sf::RenderWindow& window);
+
+  /**
+    Returns the sf::Vector2f object with the x and y sizes of the container.
+  */
   sf::Vector2f GetSize();
+
+  /**
+    Returns the sf::Vector2f object with the x and y position of the container.
+  */
   sf::Vector2f GetPosition();
+
 private:
-  sf::RectangleShape m_container;
+  sf::RectangleShape m_container; //Object holding data for the ContentContainer.
 };
