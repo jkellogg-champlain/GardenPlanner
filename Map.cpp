4b137fecffc3b682105f7dd224d9dd40118d8aa2
jkellogg-champlain <jeremiah.kellogg@mymail.champlain.edu>
2019-11-27 13:42:44 -0800

    Cleanup and comment code

--- Map.cpp
+++ Map.cpp
@@ -1,67 +1,84 @@
 #include "Map.h"
 
+//Default Constructor.
 Map::Map() { }
+
+//Default Destructor.
 Map::~Map() { }
 
+//Assigns a value to the m_mapID member variable.
 void Map::SetMapID(int id)
 {
   m_mapID = id;
 }
 
+//Returns the value assigned to the m_mapID member variable.
 int Map::GetMapID()
 {
   return m_mapID;
 }
 
+//Assigns a value to the m_name member variable.
 void Map::SetName(std::string name)
 {
   m_name = name;
 }
 
+//Returns the value assigned to the m_name member variable.
 std::string Map::GetName()
 {
   return m_name;
 }
 
+//Assigns a value to the m_year member variable.
 void Map::SetYear(std::string year)
 {
   m_year = year;
 }
 
+//Returns the value assigned to the m_year member variable.
 std::string Map::GetYear()
 {
   return m_year;
 }
 
+//Assigns a value to the m_length member variable (for x axis).
 void Map::SetLength(int length)
 {
   m_length = length;
 }
 
+//Returns the value assigned to the m_length (x axis) member variable.
 int Map::GetLength()
 {
   return m_length;
 }
 
+//Assigns a value to the m_width (y axis) member variable.
 void Map::SetWidth(int width)
 {
   m_width = width;
 }
 
+//Returns the value assigned to the m_width (y axis) member variable.
 int Map::GetWidth()
 {
   return m_width;
 }
 
+//Assigns a value to the m_isSelected variable for determining if the map is most currently selected by user.
 void Map::SetIsSelected(bool selected)
 {
   m_isSelected = selected;
 }
+
+//Returns the value assigned to the m_isSelected variable for determining if the most currently selected map should be displayed.
 bool Map::GetIsSelected()
 {
   return m_isSelected;
 }
 
+//Adds all relevant member variable data to MySQL garden_space_planner database in the maps table.
 void Map::AddToDatabase()
 {
   driver = get_driver_instance();
@@ -76,8 +93,3 @@ void Map::AddToDatabase()
   delete con;
   delete prep_stmt;
 }
-
-/*void Map::GetFromDatabase()
-{
-
-}*/
--- Map.h
+++ Map.h
@@ -10,31 +10,99 @@
 class Map
 {
 public:
+
+  //Default Constructor.
   Map();
+
+  //Default Destructor.
   ~Map();
 
+  /**
+    Assigns a value to the m_mapID member variable.
+
+    @param id Sets the map ID for the Map object.
+  */
   void SetMapID(int id);
+
+  /**
+    Returns the value assigned to the m_mapID member variable.
+  */
   int GetMapID();
+
+  /**
+    Assigns a value to the m_name member variable.
+
+    @param name Sets the map Name for the Map object.
+  */
   void SetName(std::string name);
+
+  /**
+    Returns the value assigned to the m_name member variable.
+  */
   std::string GetName();
+
+  /**
+    Assigns a value to the m_year member variable.
+
+    @param year Sets the map year for the Map object.
+  */
   void SetYear(std::string year);
+
+  /**
+    Returns the value assigned to the m_year member variable.
+  */
   std::string GetYear();
+
+  /**
+    Assigns a value to the m_length member variable (for x axis).
+
+    @param length Sets the map length (x axis) for the Map object.
+  */
   void SetLength(int length);
+
+  /**
+    Returns the value assigned to the m_length (x axis) member variable.
+  */
   int GetLength();
+
+  /**
+    Assigns a value to the m_width (y axis) member variable.
+
+    @param width Sets the map width (y axis) for the Map object.
+  */
   void SetWidth(int width);
+
+  /**
+    Returns the value assigned to the m_width (y axis) member variable.
+  */
   int GetWidth();
+
+  /**
+    Assigns a value to the m_isSelected variable for determining if the map is most currently selected by user.
+
+    @param selected The true or false value determining if the map is most currently selected by the user.
+  */
   void SetIsSelected(bool selected);
+
+  /**
+    Returns the value assigned to the m_isSelected variable for determining if the most currently selected map should be displayed.
+  */
   bool GetIsSelected();
+
+  /**
+    Adds all relevant member variable data to MySQL garden_space_planner database in the maps table.
+  */
   void AddToDatabase();
-  //void GetMapFromDatabase();
 
 private:
-  int m_mapID;
-  std::string m_name;
-  std::string m_year;
-  int m_length;
-  int m_width;
-  bool m_isSelected;
+  int m_mapID;  //Variable for holding the map's database ID.
+  std::string m_name;  //Variable for holding the map's user given name.
+  std::string m_year;  //Variable for holding the map's user given year.
+  int m_length;  //Variable for holding the map's user given length (on x axis).
+  int m_width;  //Variable for holding the map's user given width (on y axis).
+  bool m_isSelected;  //Variable holding true or false value for determining if a map has been selected by the user.
+
+  //Pointers to MySQL Connector/C++ objects needed for adding map data to the MySQL garden_space_planner database in the maps table.
   sql::Driver *driver;
   sql::Connection *con;
   sql::Statement *stmt;
